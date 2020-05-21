240d56e124109dcab7b26543ffa5c6b0883ea8ea
krunal-ctrl <50491905+krunal-ctrl@users.noreply.github.com>
2020-05-21 11:21:37 +0530

    Add files via upload

---
+++ Algorithmic-Toolbox/week2_algorithmic_warmup/Fibonacci Number/fibonacci_number.py
@@ -0,0 +1,28 @@
+# python3
+
+
+def fibonacci_number_naive(n):
+    assert 0 <= n <= 45
+
+    if n <= 1:
+        return n
+
+    return fibonacci_number_naive(n - 1) + fibonacci_number_naive(n - 2)
+
+
+def fibonacci_number(n):
+    if n <= 1:
+        return n
+
+    previous = 0
+    current  = 1
+
+    for _ in range(n - 1):
+        previous, current = current, previous + current
+
+    return current
+
+
+if __name__ == '__main__':
+    input_n = int(input())
+    print(fibonacci_number(input_n))
---
+++ Algorithmic-Toolbox/week2_algorithmic_warmup/Last Digit of Fibonacci Number/last_digit_of_fibonacci_number.py
@@ -0,0 +1,27 @@
+# python3
+
+
+def last_digit_of_fibonacci_number_naive(n):
+    assert 0 <= n <= 10 ** 7
+
+    if n <= 1:
+        return n
+
+    return (last_digit_of_fibonacci_number_naive(n - 1) + last_digit_of_fibonacci_number_naive(n - 2)) % 10
+
+
+def get_fibonacci_last_digit_fast(n):
+    if n <= 1:
+        return n
+
+    previous = 0
+    current = 1
+
+    for _ in range(n - 1):
+        previous, current = current, (previous + current) % 10
+
+    return current
+
+if __name__ == '__main__':
+    input_n = int(input())
+    print(get_fibonacci_last_digit_fast(input_n))
