7de63178e1c9d9133325817f4827bf8cc51833c8
krunal-ctrl <50491905+krunal-ctrl@users.noreply.github.com>
2020-05-21 12:24:04 +0530

    Update edit_distance.java

--- Algorithmic-Toolbox/week5_dynamic_programming1/3_edit_distance/edit_distance.java
+++ Algorithmic-Toolbox/week5_dynamic_programming1/3_edit_distance/edit_distance.java
@@ -1,33 +1,60 @@
-import java.util.*;
-import java.io.*;
-class EditDistance {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		String s = in.nextLine();
-		String[] a = s.split(" ");
-		int d = EditDistance(a[0], a[1]);
-		System.out.println(d);
-	}
-	public static int EditDistance(String a, String b) {
-		int m = a.length();
-		int n = b.length();
-		int[][] D = new int[m][n];
-		for (int i = 0; i < m; i++)
-			D[i][0] = i;
-		for (int i = 0; i < n; i++)
-			D[0][i] = i;
-		for (int j = 1; j < n; j++) {
-			for (int i = 1; i < m; i++) {
-				int ins = D[i][j-1] + 1;
-				int del = D[i-1][j] + 1;
-				int match = D[i-1][j-1];
-				int mis = D[i-1][j-1] + 1;
-				if (a.charAt(i) == b.charAt(j)) 
-					D[i][j] = Math.min(ins, Math.min(del, match));
-				else
-					D[i][j] = Math.min(ins, Math.min(del, mis));
-			}
-		}
-		return D[m - 1][n - 1];
-	}
-}
+# python3
+import numpy
+
+def EditDistance(s1, s2):
+    """ Computes the edit distance of two strings
+    (str, str) -> (int, 2D-array) """
+    ln_s1 = len(s1)
+    ln_s2 = len(s2)
+
+    # Initializing the matrix
+    Matrix = numpy.zeros((ln_s1+1 , ln_s2+1))
+    for i in range(ln_s2+1):
+        Matrix[0][i] = i
+
+    for i in range(ln_s1+1):
+        Matrix[i][0] = i
+
+    # Filling the matrix
+    for i in range(1, ln_s1+1):
+        for j in range(1, ln_s2+1):
+            insertion = Matrix[i][j-1]   + 1
+            deletion  = Matrix[i-1][j]   + 1
+            mismatch  = Matrix[i-1][j-1] + 1
+            match     = Matrix[i-1][j-1]
+            if s1[i-1] == s2[j-1]:
+                Matrix[i][j] = min(insertion, deletion, match)
+            if s1[i-1] != s2[j-1]:
+                Matrix[i][j] = min(insertion, deletion, mismatch)
+    
+    return (int(Matrix[ln_s1][ln_s2]), Matrix)
+
+def OptimalAlignment(Matrix, s1, s2, top, bottom, i, j):
+    """ Finds the optimal alignment of two strings given the edit matrix
+    (2D-array, str, str, str, str, int, int) -> (str, str) """
+
+    if i == 0 and j == 0:
+        return (' '.join(top[::-1]), ' '.join(bottom[::-1]))
+
+    if i>0 and Matrix[i][j] == Matrix[i-1][j] + 1:
+        top.append(f'|{s1[i-1]}|')
+        bottom.append('|-|')
+        return OptimalAlignment(Matrix, s1, s2, top, bottom, i-1, j)
+
+    elif j>0 and Matrix[i][j] == Matrix[i][j-1] + 1:
+        bottom.append(f'|{s2[j-1]}|')
+        top.append('|-|')
+        return OptimalAlignment(Matrix, s1, s2, top, bottom, i, j-1)
+
+    else:
+        top.append(f'|{s1[i-1]}|')
+        bottom.append(f'|{s2[j-1]}|')
+        return OptimalAlignment(Matrix, s1, s2, top, bottom, i-1, j-1)
+
+if __name__ == '__main__':
+    s1, s2 = input(), input()
+    edit_distance, Matrix = EditDistance(s1, s2)
+    top, bottom = OptimalAlignment(Matrix, s1, s2, [], [], len(s1), len(s2))
+
+    print(f'Editing distance : {edit_distance}')
+    print(f"Optimal alignment:\n{top}\n{bottom}")
